7eb6a3bbc1f3d29f1994e4f53c2c3514e1dbac48
H-LAW-48MG0\MTI <law_huisi@mti.gov.sg>
2023-11-29 16:40:00 +0800

    upload new file

---
+++ ask-economist-streamlit.py
@@ -0,0 +1,41 @@
+import os
+import streamlit as st 
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chat_models import AzureChatOpenAI
+
+
+openai_token = os.environ.get("OPENAI_TOKEN", "")
+openai_endpoint = "https://mti-nerve-openai-us-east-2.openai.azure.com/"
+
+os.environ["OPENAI_API_TYPE"] = "azure"
+os.environ["OPENAI_API_VERSION"] = "2023-12-01-preview"
+os.environ["OPENAI_API_BASE"] = openai_endpoint
+os.environ["OPENAI_API_KEY"] = openai_token    
+
+
+def create_agent_chain():
+    llm = AzureChatOpenAI(temperature=0, 
+        verbose=True, 
+        deployment_name="gpt-4",
+    )
+    chain = load_qa_chain(llm, chain_type="stuff")
+    return chain
+
+def get_llm_response(query):
+    vectordb = "./chroma_store"
+    chain = create_agent_chain()
+    matching_docs = vectordb.similarity_search(query)
+    answer = chain.run(input_documents=matching_docs, question=query)
+    return answer
+
+
+# Streamlit UI
+# ===============
+st.set_page_config(page_title="Ask Economist", page_icon=":robot:")
+st.header("Ask Economist")
+
+form_input = st.text_input('Enter Query')
+submit = st.button("Generate")
+
+if submit:
+    st.write(get_llm_response(form_input))
