dd78b44b83ee4efc91e08de919e61c211b17c2e1
H-LAW-48MG0\MTI <law_huisi@mti.gov.sg>
2023-12-06 18:48:36 +0800

    add metadata

--- ask-economist-embeddings.py
+++ ask-economist-embeddings.py
@@ -32,6 +32,8 @@ chunked_documents = text_splitter.split_documents(documents)
 client = chromadb.Client()
 if client.list_collections():
     consent_collection = client.get_or_create_collection(name="ask-economist-collection")
+    for doc in chunked_documents:
+        consent_collection.add(metadatas=doc.metadata)
 else:
     print("Collection already exists")
 vectordb = Chroma.from_documents(
--- ask-economist-streamlit.py
+++ ask-economist-streamlit.py
@@ -9,6 +9,7 @@ import streamlit as st
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import AzureChatOpenAI
 import chromadb
+from langchain.chains.query_constructor.base import AttributeInfo
 from langchain.vectorstores import Chroma
 from langchain.embeddings import AzureOpenAIEmbeddings
 from langchain.chains import ConversationalRetrievalChain
@@ -27,6 +28,14 @@ embeddings = AzureOpenAIEmbeddings(deployment="text-embedding-ada-002",chunk_siz
 dir="./chroma_store/"
 vectordb = Chroma(persist_directory=dir,embedding_function=embeddings)
 
+metadata_field_info = [
+    AttributeInfo(
+        name="source",
+        description="The answer which is from, the pdf documents name, e.g. `BA_1Q2023.pdf`",
+        type="string",
+    )
+]
+
 def create_agent_chain():
     llm = AzureChatOpenAI(temperature=0, 
         verbose=True, 
@@ -38,10 +47,11 @@ def create_agent_chain():
     #chain = load_qa_chain(llm, chain_type="stuff")
     return chain
 
-def get_llm_response(query):
+def get_llm_response(query, source_documents):
     matching_docs = vectordb.similarity_search(query)
+    matching_source = vectordb.metadata['source'](source_documents)
     chain = create_agent_chain()
-    answer = chain.run(input_documents=matching_docs, question=query)
+    answer = chain.run(input_documents=matching_docs, question=query, source=source_documents)
     return answer
 
 
