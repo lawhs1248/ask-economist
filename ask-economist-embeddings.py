23401d9f6d1440d95f90f1ecd6fd223975c3bc0a
H-LAW-48MG0\MTI <law_huisi@mti.gov.sg>
2024-01-08 16:30:07 +0800

    updated

--- ask-economist-embeddings.py
+++ ask-economist-embeddings.py
@@ -1,7 +1,7 @@
 import os
 
 from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chat_models import AzureChatOpenAI
@@ -27,13 +27,16 @@ for file in os.listdir(pdf_folder_path):
         pdf_path = os.path.join(pdf_folder_path, file)
         loader = PyPDFLoader(pdf_path)
         documents.extend(loader.load())
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
 chunked_documents = text_splitter.split_documents(documents)
+#data = [set((text.metadata["source"], embedding) for text, embedding in zip(chunked_documents, embeddings))]
+
 client = chromadb.Client()
 if client.list_collections():
     consent_collection = client.get_or_create_collection(name="ask-economist-collection")
 else:
     print("Collection already exists")
+    
 vectordb = Chroma.from_documents(
     documents=chunked_documents,
     embedding=embeddings,
--- ask-economist-streamlit.py
+++ ask-economist-streamlit.py
@@ -12,8 +12,7 @@ import chromadb
 from langchain.chains.query_constructor.base import AttributeInfo
 from langchain.vectorstores import Chroma
 from langchain.embeddings import AzureOpenAIEmbeddings
-from langchain.chains import ConversationalRetrievalChain
-
+from langchain.chains import RetrievalQAWithSourcesChain
 
 openai_token = os.environ.get("OPENAI_TOKEN", "")
 openai_endpoint = "https://mti-nerve-openai-us-east-2.openai.azure.com/"
@@ -24,33 +23,77 @@ os.environ["OPENAI_API_BASE"] = openai_endpoint
 os.environ["OPENAI_API_KEY"] = openai_token    
 
 embeddings = AzureOpenAIEmbeddings(deployment="text-embedding-ada-002",chunk_size=1)
-
 dir="./chroma_store/"
-vectordb = Chroma(persist_directory=dir,embedding_function=embeddings)
+vectordb = Chroma(persist_directory=dir,
+                  embedding_function=embeddings)
 
-def create_agent_chain():
-    llm = AzureChatOpenAI(temperature=0, 
-        verbose=True, 
-        deployment_name="gpt-4",
-    )
-    chain = load_qa_chain(llm, chain_type="stuff")
-    return chain
+llm = AzureChatOpenAI(temperature=0, 
+    seed=1,
+    verbose=True, 
+    deployment_name="gpt-4",
+)
+chain = RetrievalQAWithSourcesChain.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectordb.as_retriever(),
+    return_source_documents=True
+)
 
-def get_llm_response(query):
-    matching_docs = vectordb.similarity_search(query)
-    chain = create_agent_chain()
-    answer = chain.run(input_documents=matching_docs, question=query)
-    return answer
+# Define the 'generate_response' function to send the user's message to the AI model 
+# and append the response to the 'generated' list.
+def generate_response(prompt, conversation_chain):
+    try:
+        result = conversation_chain(prompt)
+        return result["answer"], ' '.join(list(set([doc.metadata['source'] for doc in result['source_documents']])))
+    except Exception as e:
+        print(e)
+        return "I am unable to get the response based on this question, please fine-tune it before retrying", ""
 
+# The 'chat_click' function is defined to send the user's message to the AI model 
+# and append the response to the conversation history.
+def chat_click(user_chat_input, conversation_chain):
+    if user_chat_input != '':
+        answer, sources=generate_response(user_chat_input, conversation_chain)
+        st.session_state['sources'] = []
+        st.session_state['past'] = []
+        st.session_state['answers'] = []
+        st.session_state['sources'].append(sources)
+        st.session_state['past'].append(user_chat_input)
+        st.session_state['answers'].append(answer)
 
-# Streamlit UI
-# ===============
+# Streamlit to set the page header and icon.
 st.set_page_config(page_title="Ask Economist", page_icon=":robot:")
-st.header("Ask Economist")
+st.title("Ask Economist")
+# container for text box
+container = st.container()
+# container for chat history
+response_container = st.container()
 
-form_input = st.text_input('Enter Query')
-submit = st.button("Generate")
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        st.subheader("Question: ")
+        user_input = st.text_area("Question:", key='input', height=100, label_visibility="hidden")
+        submit_button = st.form_submit_button(label='Send')
 
-if submit:
-    st.write(get_llm_response(form_input))
+    
+    if submit_button and user_input:
+        chat_click(user_input, chain)
 
+# The 'message' function is defined to display the messages in the conversation history.
+if 'answers' in st.session_state:
+    if st.session_state['answers']:
+        with response_container:
+            cols = st.columns(2)
+            for i in range(len(st.session_state['answers'])):
+                with cols[0]:
+                    st.subheader("Question: ")
+                    st.write(st.session_state['past'][i])
+                    st.subheader("Answer: ")
+                    st.write(st.session_state['answers'][i])
+                with cols[1]:
+                    st.subheader("Sources: ")
+                    for index, source in enumerate(st.session_state['source'][i].split(".pdf")):
+                        st.write(index+1, source)
+                        st.text(" ")
+            
+        # send_survey_result(st.session_state.session_id, st.session_state.nerve_logger, st.session_state['credentials_correct'], user_input)
