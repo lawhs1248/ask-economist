b1d6effd6ed2cb8251d732ca3774ca3bfe478b68
H-LAW-48MG0\MTI <law_huisi@mti.gov.sg>
2024-01-10 15:58:01 +0800

    updated

--- ask-economist-embeddings.py
+++ ask-economist-embeddings.py
@@ -9,6 +9,11 @@ from langchain.vectorstores import Chroma
 import chromadb
 from langchain.callbacks import get_openai_callback
 from langchain.chains.question_answering import load_qa_chain
+import fitz  # PyMuPDF
+from chromadb import Chroma
+import chromadb
+import openai
+import os
 
 openai_token = os.environ.get("OPENAI_TOKEN", "")
 openai_endpoint = "https://mti-nerve-openai-us-east-2.openai.azure.com/"
@@ -17,7 +22,7 @@ os.environ["OPENAI_API_TYPE"] = "azure"
 os.environ["OPENAI_API_VERSION"] = "2023-12-01-preview"
 os.environ["OPENAI_API_BASE"] = openai_endpoint
 os.environ["OPENAI_API_KEY"] = openai_token
-
+    
 embeddings = OpenAIEmbeddings(deployment="text-embedding-ada-002",chunk_size=1)
 
 pdf_folder_path = "./input"
